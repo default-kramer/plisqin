080b199c916492cbb714a1fd3a7387cdea89c8b1
Ryan Kramer <default.kramer@gmail.com>
2018-11-18 18:29:34 -0600

    set version to 0.1, maybe PLT picks this up

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/plisqin.scrbl" (multi-page))))
 (define pkg-desc "SQL Generator")
-(define version "0.0")
+(define version "0.1")
 (define pkg-authors '(Ryan Kramer))
