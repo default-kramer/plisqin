70ff17b038c8228eed1ada940f919505b9c3ae2c
Ryan Kramer <default.kramer@gmail.com>
2019-01-25 18:51:30 -0600

    add REPL support to lang plisqin

--- private/lang/reader.rkt
+++ private/lang/reader.rkt
@@ -5,9 +5,10 @@
          syntax/strip-context)
 
 (provide (except-out (all-from-out racket)
-                     #%module-begin read-syntax #%app)
+                     #%module-begin read-syntax #%app #%top-interaction)
          (rename-out [module-begin #%module-begin]
-                     [read-syn read-syntax]
+                     [top-interaction #%top-interaction]
+                     [reader read-syntax]
                      [runtime/#%app #%app]
                      [runtime/#%do-dot #%do-dot]))
 
@@ -56,23 +57,43 @@
 (define (customize-readtable)
   (make-readtable (current-readtable) #\. 'terminating-macro read-dot))
 
-(define (reader source-name in accum)
+; Returns list of syntax objects
+(define (read-all source-name in accum)
   (define result (read-syntax source-name in))
   (if (not (eof-object? result))
-      (reader source-name in (cons result accum))
-      (begin
-        (set! result `(module my-mod plisqin/private/lang/reader
-                        (require plisqin/private/lang/default-require)
-                        ,@(reverse accum)))
-        (set! result (datum->syntax #f result))
-        ; strip-context per https://groups.google.com/forum/#!topic/racket-users/DFBnCqLg0Xk
-        (set! result (strip-context result))
-        (set! result (rewriter/rewrite result))
-        result)))
+      (read-all source-name in (cons result accum))
+      (reverse accum)))
 
-(define (read-syn source-name in)
-  (parameterize ([current-readtable (customize-readtable)])
-    (reader source-name in '())))
+(define (reader source-name in)
+  (define stxs
+    (parameterize ([current-readtable (customize-readtable)])
+      (read-all source-name in '())))
+  (define result `(module my-mod plisqin/private/lang/reader
+                    (require plisqin/private/lang/default-require)
+                    (module configure-runtime racket
+                      (require (submod plisqin/private/lang/reader runtime-help))
+                      (lang-plisqin-setup!))
+                    ,@stxs))
+  (set! result (datum->syntax #f result))
+  ; strip-context per https://groups.google.com/forum/#!topic/racket-users/DFBnCqLg0Xk
+  (set! result (strip-context result))
+  (set! result (rewriter/rewrite result))
+  result)
+
+(define (read-repl source-name in)
+  (define result
+    (parameterize ([current-readtable (customize-readtable)]
+                   [read-accept-lang #f])
+      (read-syntax source-name in)))
+  (if (eof-object? result)
+      result
+      ; TODO Not sure if strip-context is desired here...
+      (rewriter/rewrite (strip-context result))))
+
+(module+ runtime-help
+  (provide lang-plisqin-setup!)
+  (define (lang-plisqin-setup!)
+    (current-read-interaction read-repl)))
 
 (define-syntax (module-begin stx)
   (define result
@@ -82,3 +103,16 @@
   (define transformed result); (rewriter/rewrite result))
   ;(println transformed)
   transformed)
+
+(define-syntax (top-interaction stx)
+  ; If we type {a = b} into the REPL, we will get
+  ;   {#%top-interaction a = b}
+  ; Before we pass it to the rewriter, we want to transform it back to
+  ;   {a = b}
+  (set! stx (datum->syntax stx
+                           (cdr (syntax-e stx))
+                           stx stx))
+  ;(println stx)
+  (set! stx (rewriter/rewrite stx))
+  ;(println stx)
+  #`(#%top-interaction . #,stx))
