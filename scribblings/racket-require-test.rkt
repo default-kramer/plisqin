93bde55256911fa780ff6a6ae44c2062b7e65527
Ryan Kramer <default.kramer@gmail.com>
2019-02-09 13:15:30 -0600

    fix clashes in require for-label racket.rkt

---
+++ scribblings/racket-require-test.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+; The point of "racket.rkt" is to provide all the imports
+; that don't clash with plisqin/private/lang/default-require.
+; This file simply tests that this does not error:
+(require (for-label plisqin/private/lang/default-require
+                    "racket.rkt"))
--- scribblings/racket.rkt
+++ scribblings/racket.rkt
@@ -1,14 +1,12 @@
 #lang racket
 ; Provide everything from racket except identifiers that clash with plisqin.
+(define-for-syntax plisqin-conflicts
+  (list "group-by" "count" "min" "max" "second"
+        "+" "-" "/" "*" "=" "<" "<=" ">" ">="))
 
-; For some reason, this doesn't hide stuff from racket/base:
-(void '(provide (except-out (all-from-out racket)
-                            group-by count min max)))
-
-; So do it this way instead:
 (require racket/provide)
 (provide (filtered-out
-          (λ(name) (if (member name '("group-by" "count" "min" "max"))
+          (λ(name) (if (member name plisqin-conflicts)
                        #f
                        name))
           (all-from-out racket)))
