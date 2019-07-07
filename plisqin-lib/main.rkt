33fa613d8cf7de5821a42b73c69370520c7e7051
Ryan Kramer <default.kramer@gmail.com>
2019-07-07 11:40:17 -0500

    fix lang plisqin

--- plisqin-lib/main.rkt
+++ plisqin-lib/main.rkt
@@ -38,7 +38,3 @@
 (module+ main
   ;; Main entry point, executed when run with the `racket` executable or DrRacket.
   )
-
-(module reader racket
-  (require "private/lang/reader.rkt")
-  (provide read-syntax))
--- plisqin/main.rkt
+++ plisqin/main.rkt
@@ -2,3 +2,7 @@
 
 (require plisqin-lib)
 (provide (all-from-out plisqin-lib))
+
+(module reader racket
+  (require plisqin-lib/private/lang/reader)
+  (provide read-syntax))
