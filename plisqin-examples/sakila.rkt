516c4b6be071b5a518dd1d2211ce8c322ba35719
Ryan Kramer <default.kramer@gmail.com>
2019-07-14 14:54:28 -0500

    add plisqin-examples

---
+++ plisqin-examples/sakila.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+
+(require "sakila/main.rkt")
+(provide (all-from-out "sakila/main.rkt"))
---
+++ plisqin-examples/sakila/rebuild-sqlite.bat
@@ -0,0 +1,3 @@
+del sakila.db
+sqlite3.exe sakila.db < sakila-schema.sqlite.sql
+sqlite3.exe sakila.db < sakila-data.sqlite.sql
