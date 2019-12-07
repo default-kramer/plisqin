9ccb70ad4349b6b70af9288a1796c040cc01f677
Ryan Kramer <default.kramer@gmail.com>
2019-12-06 18:51:48 -0600

    remove some obsolete code

--- plisqin-lib/operators.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket/base
-
-(require plisqin-lib/private/operators)
-(provide (all-from-out plisqin-lib/private/operators))
--- plisqin-lib/structs.rkt
+++
@@ -1,3 +0,0 @@
-#lang racket
-(require (submod "private/model.rkt" all))
-(provide (all-from-out (submod "private/model.rkt" all)))
