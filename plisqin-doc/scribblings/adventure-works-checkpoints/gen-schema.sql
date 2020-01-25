a1187c8ba4124153c292931496f200bb75633cbc
Ryan Kramer <default.kramer@gmail.com>
2020-01-25 15:59:56 -0600

    add the SQL query I used to generate the initial schema

---
+++ plisqin-doc/scribblings/adventure-works-checkpoints/gen-schema.sql
@@ -0,0 +1,24 @@
+-- This is specific to SQL Server
+select '(table '
+	+ t.TABLE_NAME
+	+ ' #:column '
+	+ (select '['
+			+ c.COLUMN_NAME
+			+ case when c.DATA_TYPE like '%int' or c.DATA_TYPE like '%decimal' or c.DATA_TYPE like '%money'
+					or c.DATA_TYPE like '%numeric' then ' #:type Number'
+				when c.DATA_TYPE like '%char' then ' #:type String'
+				when c.DATA_TYPE in ('datetime', 'date') then ' #:type Datetime'
+				else '' end
+			+ '] '
+			as 'data()'
+		from INFORMATION_SCHEMA.COLUMNS c
+		where c.TABLE_NAME = t.TABLE_NAME
+		and c.TABLE_SCHEMA = t.TABLE_SCHEMA
+		order by c.COLUMN_NAME
+		for xml path('')
+	)
+	+ ')' as RacketCode
+from INFORMATION_SCHEMA.TABLES t
+where t.TABLE_TYPE = 'BASE TABLE'
+and t.TABLE_SCHEMA <> 'dbo'
+order by t.TABLE_SCHEMA, t.TABLE_NAME
