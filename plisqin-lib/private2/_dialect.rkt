6dced15ab925d8caceac8cc8c1892b2f1e75cd75
Ryan Kramer <default.kramer@gmail.com>
2020-03-09 19:32:29 -0500

    cleanup

--- plisqin-lib/private2/_dialect.rkt
+++ plisqin-lib/private2/_dialect.rkt
@@ -1,4 +1,16 @@
-#lang racket/base
+#lang racket
 
-(require morsel-lib/sql/dialect)
 (provide (all-from-out morsel-lib/sql/dialect))
+
+(require morsel-lib/sql/dialect
+         (prefix-in db: db))
+
+; TODO decide if this code is still useful:
+(define (infer-dialect x)
+  #;(-> (or/c db:connection? db:dbsystem?) (or/c dialect? #f))
+  (if (db:connection? x)
+      (infer-dialect (db:connection-dbsystem x))
+      (match (db:dbsystem-name x)
+        ['postgresql (postgres)]
+        ['sqlite3 (sqlite)]
+        [else #f])))
