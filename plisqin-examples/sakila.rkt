46edf74a69131998bbccf971b4cab611cd8a71b4
Ryan Kramer <default.kramer@gmail.com>
2020-03-18 18:53:02 -0500

    remove sakila

--- plisqin-examples/sakila.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket/base
-
-(require "sakila/main.rkt")
-(provide (all-from-out "sakila/main.rkt"))
--- plisqin-examples/sakila/rebuild-sqlite.bat
+++
@@ -1,3 +0,0 @@
-del sakila.db
-sqlite3.exe sakila.db < sakila-schema.sqlite.sql
-sqlite3.exe sakila.db < sakila-data.sqlite.sql
