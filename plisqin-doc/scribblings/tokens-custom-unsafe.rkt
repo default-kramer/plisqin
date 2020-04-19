c5166d63a0547ce35a476b69132a4b3baa725971
Ryan Kramer <default.kramer@gmail.com>
2020-04-19 15:13:52 -0500

    tweak docs on ??

--- plisqin-doc/scribblings/tokens-custom-unsafe.rkt
+++ plisqin-doc/scribblings/tokens-custom-unsafe.rkt
@@ -21,37 +21,35 @@
  The token's @tech{nullability} is always @(racket no).
  })
    (define (doc-??)
-     @defthing[#:kind "procedure" ?? procedure?]{
+     @defform[#:kind "procedure" (?? token-1 [token-N ...+] [/fallback])
+              #:contracts ([token-1 Token]
+                           [token-N Token]
+                           [/fallback fallback?])]{
  This procedure is typically used to attach a @tech{fallback}, but it can also be
  used as a synonym for @(racket %%coalesce) if no fallback is given.
- The general form of this procedure is:
- @(racketblock
-   (?? token [maybe-more-tokens ...] [maybe-fallback]))
- When @(racket maybe-more-tokens) are given, @(racket %%coalesce) is used.
- When @(racket maybe-fallback) is given, it is attached.
- To be more specific, this procedure has 3 cases:
+ More precisely, this procedure has 3 cases:
  @(itemlist
    #:style 'ordered
    @item{When given two arguments and the last argument is a @(racket fallback?),
   it simply attaches the fallback to the first argument:
   @(racketblock
-    (?? token /fallback)
+    (?? token-1 /fallback)
     (code:comment "is equivalent to")
-    (>> token #:fallback /fallback))}
+    (>> token-1 #:fallback /fallback))}
    @item{When given more than two arguments and the last argument is a @(racket fallback?),
   it attaches the fallback to the result of @(racket %%coalesce):
   @(racketblock
-    (?? token-1 .. token-N /fallback)
+    (?? token-1 token-N ...+ /fallback)
     (code:comment "is equivalent to")
-    (>> (%%coalesce token-1 .. token-N) #:fallback /fallback))}
+    (>> (%%coalesce token-1 token-N ...+) #:fallback /fallback))}
    @item{When the last argument is not a @(racket fallback?), it is simply a
   synonym for @(racket %%coalesce):
   @(racketblock
-    (?? token-1 .. token-N)
+    (?? token-1 token-N ...+)
     (code:comment "is equivalent to")
-    (%%coalesce token-1 .. token-N))})
+    (%%coalesce token-1 token-N ...+))})
 
  You may need to check the documentation on @(racket %%coalesce) to know the return
- value of cases 2 and 3.
+ type of cases 2 and 3.
  })
    )
